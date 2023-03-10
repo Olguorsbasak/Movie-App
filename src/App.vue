e661b3b72eaaca9a180bccdb9dbee9cb9213c480
Olgu Örs Başak <olguors@gmail.com>
2023-03-10 15:32:03 +0300

    Added main change

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script >
+import Movie from './components/Movie.vue'
+export default 
+{
+  name: 'App',
+  components: {
+    Movie
+  },
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <Movie msg="Welcome to Find Your Movie App"/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
